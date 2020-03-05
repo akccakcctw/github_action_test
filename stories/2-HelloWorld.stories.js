fe44eb3fb53e145f450c6f0edbf33c99fd3a2cf8
Rex Tsou <rex.tsou@kkday.com>
2020-03-05 13:37:44 +0800

    Add story HelloWorld

---
+++ stories/2-HelloWorld.stories.js
@@ -0,0 +1,13 @@
+import HelloWorld from '../src/components/HelloWorld.vue';
+
+export default {
+  title: 'HelloWorld',
+  component: HelloWorld,
+};
+
+export const Default = () => ({
+  components: { HelloWorld },
+  template: `
+    <hello-world />
+  `,
+});
